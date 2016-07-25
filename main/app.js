f567d34e7e1301a9aaa4aa0b345ec49d8b073650
liyanyan19940914 <921258351@qq.com>
2016-07-25 19:38:09 +0800

    move buildCartItems function to CartItem.buildCartItems method

--- main/app.js
+++ main/app.js
@@ -5,7 +5,7 @@ const Item=require('./models/item.js');
 const Promotion=require('./models/promotion.js');
 function printReceipt(tags) {
 
-    const cartItems = buildCartItems(tags, Item.all());
+    const cartItems = CartItem.buildCartItems(tags, Item.all());
 
     const receiptItems = buildReceiptItems(cartItems, Promotion.all());
 
@@ -16,28 +16,6 @@ function printReceipt(tags) {
     console.log(receiptText);
 }
 
-function buildCartItems(tags, allItems) {
-
-    const cartItems = [];
-
-    for (const tag of tags) {
-
-        const tagArray = tag.split('-');
-        const barcode = tagArray[0];
-        const count = parseFloat(tagArray[1] || 1);
-
-        const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
-
-        if (cartItem) {
-            cartItem.count += count;
-        } else {
-            const item = allItems.find(item => item.barcode === barcode);
-            cartItems.push(new CartItem(item,count));
-        }
-    }
-
-    return cartItems;
-}
 
 function buildReceiptItems(cartItems, allPromotions) {
     return cartItems.map(cartItem => {
