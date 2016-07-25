994374750368b319c43a64b02c0fef1ae3b9bcf6
liyanyan19940914 <921258351@qq.com>
2016-07-25 18:21:33 +0800

    creat pos_v1 origin code

--- run-spec.js
+++ run-spec.js
@@ -1,5 +1,5 @@
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
-jasmine.loadConfigFile('../spec/support/jasmine.json');
+jasmine.loadConfigFile('spec/support/jasmine.json');
 
 jasmine.execute();
