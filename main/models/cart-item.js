b42f721fabddc749ff58d0e7a430dc9494ee4cac
liyanyan19940914 <921258351@qq.com>
2016-07-25 20:11:58 +0800

    refactor CartItem

--- main/models/cart-item.js
+++ main/models/cart-item.js
@@ -4,6 +4,18 @@ class CartItem{
         this.item=item;
         this.count=count;
     }
+    getBarcode(){
+        return this.item.barcode;
+    }
+    getName(){
+        return this.item.name;
+    }
+    getUnit(){
+        return this.item.unit;
+    }
+    getPrice(){
+        return this.item.price;
+    }
     static buildCartItems(tags, allItems) {
 
     const cartItems = [];
@@ -14,7 +26,7 @@ class CartItem{
         const barcode = tagArray[0];
         const count = parseFloat(tagArray[1] || 1);
 
-        const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+        const cartItem = cartItems.find(cartItem => cartItem.getBarcode() === barcode);
 
         if (cartItem) {
             cartItem.count += count;
